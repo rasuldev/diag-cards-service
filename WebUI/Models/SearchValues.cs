b5940559b3d3e75332c6604e9bf746e74f7da64f
Rustam M-K <m-rustam1996@rambler.ru>
2017-08-24 12:25:28 +0300

    add file to git

---
+++ WebUI/Models/SearchValues.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class SearchValues
+    {
+        [Display(Name = "RegNumber")]
+        [RegularExpression(@"[А-Я]\d{3}[А-Я]{2}\d{2}", ErrorMessage = "Введите корректный регистрационный номер")]
+        public string RegNumber { get; set; }
+
+        [Display(Name = "VIN")]
+        [RegularExpression(@"[А-ЯA-Z0-9]", ErrorMessage = "Введите корректный VIN")]
+        public string VIN { get; set; }
+
+        [Display(Name = "FIO")]
+        [RegularExpression(@"[а-яА-Яa-zA-Z^0-9]*", ErrorMessage = "Введите корректное имя")]
+        public string FIO { get; set; }
+
+        [Display(Name = "StartDate")]
+        [DataType(DataType.Date, ErrorMessage = "Укажите корректную дату")]
+        public DateTime StartDate = new DateTime(1, 1, 1);
+
+        [Display(Name = "EndDate")]
+        [DataType(DataType.Date, ErrorMessage = "Укажите корректную дату")]
+        public DateTime EndDate = DateTime.Now;
+    }
+}
