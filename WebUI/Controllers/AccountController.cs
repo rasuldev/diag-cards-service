7408ff82e7284b7b969cd5c6b6e0e933bb1275da
Rustam M-K <m-rustam1996@rambler.ru>
2017-08-25 16:34:08 +0300

    fix sign in for removed users

--- WebUI/Controllers/AccountController.cs
+++ WebUI/Controllers/AccountController.cs
@@ -68,6 +68,9 @@ namespace WebUI.Controllers
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user.IsRemoved)
+                    return View("Lockout");
                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
