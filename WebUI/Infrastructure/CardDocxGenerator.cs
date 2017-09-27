76c17e8eef5d2a89981d48ab08723c537c5adc77
Rasul <rasuldev@gmail.com>
2017-09-27 20:09:12 +0300

    generating blank completed

--- WebUI/Infrastructure/CardDocxGenerator.cs
+++ WebUI/Infrastructure/CardDocxGenerator.cs
@@ -17,7 +17,7 @@ namespace WebUI.Infrastructure
         public async Task<Stream> Generate(DiagnosticCard card)
         {
             var memoryStream = new MemoryStream();
-            using (var fs = new FileStream(_templateFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var fs = new FileStream(_templateFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
                 await fs.CopyToAsync(memoryStream);
             }
@@ -25,11 +25,24 @@ namespace WebUI.Infrastructure
             using (var docx = DocX.Load(memoryStream))
             {
                 void Replace(string oldVal, string newVal) => docx.ReplaceText(oldVal, newVal?.ToUpper() ?? "");
+                // Replaces text #<start>#, #<start+1>#,..., "<end>" with chars from value
+                void ReplaceSeq(int start, int end, string value)
+                {
+                    if (string.IsNullOrEmpty(value)) return;
+                    for (int i = start; i <= end; i++)
+                    {
+                        Replace($"#{i}#", value[i - start].ToString());
+                    }
+                }
+
+                ReplaceSeq(1, 15, card.CardId);
+                ReplaceSeq(20, 27, card.ExpirationDate.ToString("ddMMyyyy"));
+                ReplaceSeq(30, 37, card.RegisteredDate?.ToString("ddMMyyyy"));
                 Replace("#reg_znak#", card.RegNumber);
                 Replace("#vin#", card.VIN);
                 Replace("#rama#", card.FrameNumber);
                 Replace("#kuzov#", card.BodyNumber);
-                Replace("#vidano#", $"{card.DocumentSeries}, {card.DocumentNumber}, выдан {card.DocumentIssuer} {card.DocumentIssueDate:dd.MM.yyyy}");
+                Replace("#vidano#", $"{card.DocumentSeries} {card.DocumentNumber} {card.DocumentIssuer} {card.DocumentIssueDate:dd.MM.yyyy}");
                 Replace("#marka#", $"{card.Manufacturer} {card.Model}");
                 Replace("#kategor1#", card.Category.ToString());
                 Replace("#god_vipuska#", card.IssueYear.ToString());
