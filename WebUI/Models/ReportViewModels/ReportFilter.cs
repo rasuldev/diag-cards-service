cc8a1886b4cd5d692e38651894a13dc527fc517d
Rasul <rasuldev@gmail.com>
2018-08-08 17:05:25 +0300

    fixed date field in chrome

--- WebUI/Models/ReportViewModels/ReportFilter.cs
+++ WebUI/Models/ReportViewModels/ReportFilter.cs
@@ -6,8 +6,10 @@ namespace WebUI.Models.ReportViewModels
     public class ReportFilter
     {
         //[Required(ErrorMessage = "Required"), Display(Name = "'Дата регистрации: от'")]
+        [DataType(DataType.Text)]
         public DateTime? Start { get; set; }
         //[Required(ErrorMessage = "Required"), Display(Name = "'Дата регистрации: до'")]
+        [DataType(DataType.Text)]
         public DateTime? End { get; set; }
     }
 }
